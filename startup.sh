4aeeb262d95038abf2e86573c2c0944a1799dde5
Frederick Henderson <frederickjh@henderson-meier.org>
2015-09-25 15:05:51 +0200

    Added copying of bash configuration files

--- startup.sh
+++ startup.sh
@@ -39,6 +39,18 @@ copy_dot_drush ()
   fi
 }
 
+# Copy bash configuration files from artificial $HOME in docker environment
+# @param $1 path to the home directory (parent of the .drush directory)
+copy_dot_bash ()
+{
+  local path="$1/"
+  if [ -d $path ]; then
+    echo "Copying Bash settings in $path from artificial \$HOME directory..."
+    cp -r $path/. ~
+  fi
+}
+
+
 # Copy SSH keys from host if available
 copy_ssh_key '/.home/.ssh' # Generic
 copy_ssh_key '/.home-linux/.ssh' # Linux (docker-compose)
@@ -54,6 +66,10 @@ copy_dot_drush '/.home' # Generic
 copy_dot_drush '/.home-linux' # Linux (docker-compose)
 copy_dot_drush '/.home-b2d' # boot2docker (docker-compose)
 copy_dot_drush '/.home-localdev'   # Drush overrides from local environment home folder
+
+# Copy Bash settings from artificial $HOME folder if available
+copy_dot_bash '/.home-localdev'
+mv  {.b,.dr,.p}* /.home-localdev ~ 2>/dev/null 
 
 echo "PHP5-FPM with environment variables"
 # Update php5-fpm with access to Docker environment variables
