3250ce747e4dc932a93df47c1866ea53503091ba
Frederick Henderson <frederickjh@henderson-meier.org>
2016-03-17 19:54:42 +0100

    Added copying of authorized_keys file from the docker_projects_home_folder so that we can be authorized on the containers for ssh login. Changed some formating for functions. Also replace some instances were the sshkey name was hardcoded in to the variable.

--- startup.sh
+++ startup.sh
@@ -6,22 +6,20 @@ echo "Using SSH key name: $SSH_KEY_NAME"
 
 # Copy SSH key pairs.
 # @param $1 path to .ssh folder
-copy_ssh_key ()
-{
+copy_ssh_key (){
   local path="$1/$SSH_KEY_NAME"
   if [ -f $path ]; then
     echo "Copying SSH key $path from host..."
-    cp $path ~/.ssh/id_rsa
-    cp ${path}.pub ~/.ssh/id_rsa.pub
-    chmod 600 ~/.ssh/id_rsa
-    chmod 644 ~/.ssh/id_rsa.pub
+    cp $path ~/.ssh/$SSH_KEY_NAME
+    cp ${path}.pub ~/.ssh/$SSH_KEY_NAME.pub
+    chmod 600 ~/.ssh/$SSH_KEY_NAME
+    chmod 644 ~/.ssh/$SSH_KEY_NAME.pub
   fi
 }
 
 # Copy Acquia Cloud API credentials
 # @param $1 path to the home directory (parent of the .acquia directory)
-copy_dot_acquia ()
-{
+copy_dot_acquia (){
   local path="$1/.acquia/cloudapi.conf"
   if [ -f $path ]; then
     echo "Copying Acquia Cloud API settings in $path from host..."
@@ -32,8 +30,7 @@ copy_dot_acquia ()
 
 # Copy Drush settings from host
 # @param $1 path to the home directory (parent of the .drush directory)
-copy_dot_drush ()
-{
+copy_dot_drush (){
   local path="$1/.drush"
   if [ -d $path ]; then
     echo "Copying Drush settings in $path from host..."
@@ -43,15 +40,21 @@ copy_dot_drush ()
 
 # Copy bash configuration files from artificial $HOME in docker environment
 # @param $1 path to the home directory (parent of the .drush directory)
-copy_dot_bash ()
-{
+copy_dot_bash (){
   local path="$1/"
   if [ -d $path ]; then
     echo "Copying Bash settings in $path from artificial \$HOME directory..."
     cp -r $path/. ~
   fi
 }
-
+# Copy authorized_keys from docker host.
+copy_authorized_keys (){
+   local path="$1/authorized_keys"
+  if [ -f $path ]; then
+    echo "Copying $path from host..."
+    cp $path ~/.ssh/authorized_keys
+  fi 
+}
 
 # Copy SSH keys from host if available
 copy_ssh_key '/.home/.ssh' # Generic
@@ -75,6 +78,14 @@ cp  /.home-localdev/{.b,.dr,.p}*  ~ 2>/dev/null
 
 # Copy scripts from artificial $HOME folder if available
 cp  /.home-localdev/bin/*  ~/bin/ 2>/dev/null
+
+# Copy authorized_keys from docker projects home folder.
+# Place your authorized_keys file one folder below the project folders.
+# There is a link to this folder which will be copied into the container.
+# symlink to ../../../authorized_keys  is located at
+# <docker_projects_home_folder>/<projectname>/.home-localdev/.ssh/authorized_keys
+# so this links to the authorized_keys file in the <docker_projects_home_folder>.
+copy_authorized_keys '/.home-localdev/.ssh'
 
 echo "PHP5-FPM with environment variables"
 # Update php5-fpm with access to Docker environment variables
